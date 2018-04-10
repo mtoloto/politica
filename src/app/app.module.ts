171027901c4ae410d02352e585a0015ddc8b09ff
Marcus Vinicius Ramos Toloto <tolotoipupo@hotmail.com>
2018-04-10 13:03:16 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,45 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ParlamentsPage, PopoverPage } from '../pages/parlaments/parlaments';
+import { ParlamentListItemPage } from '../pages/parlaments/parlament-list-item/parlament-list-item';
+import { SenatorListItem } from '../pages/parlaments/senator-list-item/senator-list-item';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { ParlamentProvider } from '../providers/parlament/parlament';
+
+ 
+import { ParlamentModalModule } from '../pages/parlaments/parlament-modal/parlament-modal.module';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ParlamentsPage,
+    PopoverPage,
+    ParlamentListItemPage,
+    SenatorListItem
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
+    ParlamentModalModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ParlamentsPage,
+    PopoverPage,
+    ParlamentListItemPage,
+    SenatorListItem
   ],
   providers: [
     StatusBar,
+    HttpClient,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    ParlamentProvider
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ParlamentsPage } from '../parlaments/parlaments';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  slides = [
+    {
+      image: "assets/imgs/background1.jpg",
+      credit: 'Foto - Lula Marques / AGPT'
+    },
+    {
+      image: "assets/imgs/background2.jpg",
+      credit: 'Foto - Ana Luiza Souza'
+    },
+    {
+      image: "assets/imgs/background3.jpg",
+      credit: 'Foto - Lula Marques / AGPT'
+    }
+  ];
+  constructor(public navCtrl: NavController) { 
+  }
 
-  constructor(public navCtrl: NavController) {
 
+  goToParlaments(type){
+    this.navCtrl.push(ParlamentsPage);
   }
 
 }
